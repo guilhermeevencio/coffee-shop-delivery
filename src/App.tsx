0c91e2ddb5c1e3157d09f35aea6f852e6f2f78f2
Guilherme <guilhermeevencioluz@gmail.com>
2022-09-07 13:44:20 -0300

    style: Set styled components configs, defaultTheme and global style.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default App
